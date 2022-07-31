5a0b041679e9b9c1277e5eacfd14fdac98fe5184
Neil Holley <neil@steelweaver.net>
2022-07-31 15:26:58 -0400

    Add list for works

--- bibliognost/tui/app.py
+++ bibliognost/tui/app.py
@@ -3,13 +3,14 @@ from curses import window, curs_set
 from sqlalchemy import select
 from sqlalchemy.orm import sessionmaker
 
-from bibliognost.persistence import Collection
+from bibliognost.persistence import Collection, Work
 from bibliognost.tui import views
 
 
 class Application:
     def __init__(self, Session: sessionmaker):
         self._session = Session
+        self._viewport = None
 
     def __call__(self, stdscr: window):
         stdscr.clear()
@@ -19,25 +20,38 @@ class Application:
         stdscr.refresh()
 
         max_y, max_x = stdscr.getmaxyx()
-        viewport = stdscr.derwin(max_y - 2, max_x - 2, 1, 1)
-        view = self._open_collections_list(viewport)
+        self._viewport = stdscr.derwin(max_y - 2, max_x - 2, 1, 1)
+        view = self._open_collections_list()
         view.refresh()
 
         while True:
             key = stdscr.getkey()
             if key == "q":
-                exit(0)
-            if key == "j":
+                break
+            elif key == "j":
                 view.next()
-            if key == "k":
+            elif key == "k":
                 view.previous()
+            elif key == "c":
+                view = self._open_collections_list()
+            elif key == "w":
+                view = self._open_works_list()
             view.refresh()
 
-    def _open_collections_list(self, stdscr: window) -> views.CollectionList:
+    def _open_collections_list(self) -> views.CollectionList:
         query = select(Collection).order_by(Collection.title)
         with self._session.begin() as session:
             collections = [
                 views.CollectionListItem(id=c.collection_id, title=c.title)
                 for c in session.execute(query).scalars().all()
             ]
-            return views.CollectionList(stdscr, collections)
+            return views.CollectionList(self._viewport, collections)
+
+    def _open_works_list(self) -> views.WorkList:
+        query = select(Work).order_by(Work.title)
+        with self._session.begin() as session:
+            works = [
+                views.WorkListItem(id=w.work_id, title=w.title)
+                for w in session.execute(query).scalars().all()
+            ]
+            return views.WorkList(self._viewport, works)
--- bibliognost/tui/views.py
+++ bibliognost/tui/views.py
@@ -1,6 +1,123 @@
+import math
 from curses import A_STANDOUT, window
 from dataclasses import dataclass
 from typing import List
+
+
+class _PagedList:
+    def __init__(self, items, page_size: int):
+        self._items = items
+        self._current_page = 0
+        self._page_size = page_size
+
+    @property
+    def total_pages(self) -> int:
+        return math.ceil(len(self._items) / self._page_size)
+
+    @property
+    def current_page(self) -> int:
+        """The current page number"""
+        return self._current_page
+
+    @current_page.setter
+    def current_page(self, value: int):
+        if value in range(self.total_pages):
+            self._current_page = value
+
+    @property
+    def current_items(self) -> List:
+        """Items on the current page"""
+        low = self.current_page * self._page_size
+        high = low + self._page_size
+        return self._items[low:high]
+
+    @property
+    def has_next(self) -> bool:
+        return self.current_page < (self.total_pages - 1)
+
+    @property
+    def has_previous(self) -> bool:
+        return self.current_page > 0
+
+
+class _PagedListView:
+    def __init__(self, win: window, items, item_repr):
+        self._win = win
+        self._item_repr = item_repr
+        self._paged_items = _PagedList(items, self.page_size)
+        self._ix_relative = 0
+        self._display_page()
+
+    def _display_page(self):
+        self._win.clear()
+        for ix, item in enumerate(self._paged_items.current_items):
+            self._win.addstr(ix, 0, self._item_repr(item))
+        self._highlight_current()
+
+    @property
+    def current(self) -> int:
+        """Index of the currently row"""
+        return self._ix_relative
+
+    @current.setter
+    def current(self, value: int):
+        if value != self._ix_relative:
+            if value < 0:
+                self._paged_items.current_page -= 1
+                self._ix_relative = self._paged_items._page_size - 1
+                self._display_page()
+            elif value >= self.page_size:
+                self._paged_items.current_page += 1
+                self._ix_relative = 0
+                self._display_page()
+            else:
+                self._reset_current()
+                self._ix_relative = value
+                self._highlight_current()
+
+    @property
+    def page_size(self) -> int:
+        max_y, _ = self._win.getmaxyx()
+        return max_y
+
+    def _repr_for(self, ix: int) -> str:
+        return self._item_repr(self._paged_items.current_items[ix])
+
+    def _reset_current(self):
+        """Remove highlighting from the current line"""
+        if self._paged_items.total_pages == 0:
+            return
+
+        self._win.addstr(self.current, 0, self._repr_for(self.current))
+
+    def _highlight_current(self):
+        """Add highlighting to the current line"""
+        if self._paged_items.total_pages == 0:
+            return
+
+        self._win.addstr(self.current, 0, self._repr_for(self.current), A_STANDOUT)
+
+    @property
+    def has_next(self) -> bool:
+        return (
+            self._ix_relative < (len(self._paged_items.current_items) - 1)
+            or self._paged_items.has_next
+        )
+
+    @property
+    def has_previous(self) -> bool:
+        return self._ix_relative > 0 or self._paged_items.has_previous
+
+    def refresh(self):
+        self._win.refresh()
+
+    def next(self):
+        if self.has_next:
+            self.current += 1
+
+    def previous(self):
+        if self.has_previous:
+            self.current -= 1
 
 
 @dataclass
@@ -10,62 +127,56 @@ class CollectionListItem:
 
 
 class CollectionList:
-    def __init__(self, win: window, collections: List[CollectionListItem]):
-        self._win = win
-        self._collections = collections
-        self.__current_ix = 0
-        max_y, _ = self._win.getmaxyx()
-        for ix, item in enumerate(self._collections):
-            self._win.addstr(ix, 0, item.title)
-            if ix >= max_y - 1:
-                break
-        self._highlight_current()
-
-    @property
-    def _current(self):
-        return self.__current_ix
-
-    @_current.setter
-    def _current(self, value: int):
-        if value != self.__current_ix:
-            self._reset_current()
-            self.__current_ix = value
-            self._highlight_current()
-
-    @property
-    def _maxy(self) -> int:
-        max_y, _ = self._win.getmaxyx()
-        return max_y
-
-    def _reset_current(self):
-        if len(self._collections) == 0:
-            return
-
-        self._win.addstr(self.__current_ix, 0, self._collections[self._current].title)
-
-    def _highlight_current(self):
-        if len(self._collections) == 0:
-            return
-
-        self._win.addstr(
-            self.__current_ix, 0, self._collections[self._current].title, A_STANDOUT
-        )
+    def __init__(self, win: window, works: List[CollectionListItem]):
+        self._view = _PagedListView(win, works, lambda it: it.title)
 
     @property
     def has_next(self):
-        return self._current < len(self._collections) - 1
+        return self._view.has_next
 
     @property
     def has_previous(self):
-        return self._current > 0
+        return self._view.has_previous
 
     def refresh(self):
-        self._win.refresh()
+        self._view.refresh()
 
     def next(self):
-        if self.has_next:
-            self._current += 1
+        self._view.next()
 
     def previous(self):
-        if self.has_previous:
-            self._current -= 1
+        self._view.previous()
+
+
+@dataclass
+class WorkListItem:
+    id: int
+    title: str
+
+
+class WorkList:
+    def __init__(self, win: window, works: List[WorkListItem]):
+        self._view = _PagedListView(win, works, lambda w: w.title)
+
+    @property
+    def has_next(self):
+        return self._view.has_next
+
+    @property
+    def has_previous(self):
+        return self._view.has_previous
+
+    def refresh(self):
+        self._view.refresh()
+
+    def next(self):
+        self._view.next()
+
+    def next_page(self):
+        self._view.next_page()
+
+    def previous(self):
+        self._view.previous()
+
+    def previous_page(self):
+        self._view.previous_page()
